860ee7ae3ac5a0775c197f766f55ad14c8c828d3
牛志伟 <niuzhiwei@mininglamp.com>
2019-04-28 17:25:54 +0800

    eslint prettier

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -2,6 +2,6 @@ import React, { PureComponent } from 'react'
 
 export default class Home extends PureComponent {
     render() {
-        return <div>我是钢铁侠</div>
+        return <div>Home</div>
     }
 }
