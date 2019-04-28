404c72c3870b9baf9f3658fd47e5b1a7bb21f185
牛志伟 <niuzhiwei@mininglamp.com>
2019-04-28 17:33:02 +0800

    eslint prettier

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -2,6 +2,6 @@ import React, { PureComponent } from 'react'
 
 export default class Home extends PureComponent {
     render() {
-        return <div>Home2</div>
+        return <div>Home</div>
     }
 }
