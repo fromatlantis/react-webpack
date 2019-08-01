861a1feeded79c79c7e8986ec50260070e2f2c67
zhaochenlei <2673355193@qq.com>
2019-08-01 11:13:18 +0800

    '统计'

--- src/components/Charts/index.js
+++ src/components/Charts/index.js
@@ -1,6 +1,5 @@
 import LineChart from './LineChart/LineChart'
 import BarChart from './BarChart/BarChart'
-import LineChart from './LineChart/LineChart'
 import Liquid from './Liquid/Liquid'
 import PictorialBar from './PictorialBar/PictorialBar'
 import PieChart from './PieChart/PieChart'
