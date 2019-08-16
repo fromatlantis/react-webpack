97d39c5465ccc92a6768b76b615f5f6cffe98fd9
牛志伟 <niuzhiwei@mininglamp.com>
2019-08-16 19:14:19 +0800

    APPID

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const origin = window.location.origin
 export const APPID =
-    process.env.CONFIG_ENV === 'development' ? 'HZYYGLPTQYFW0064' : 'HZYYGLPTWYFW0065'
+    process.env.CONFIG_ENV === 'development' ? 'HZYYGLPTQYFW0064' : 'HZYYGLPTQYFW0029'
 export default {
     origin,
     sso: `${origin}/portal/#/login`,
